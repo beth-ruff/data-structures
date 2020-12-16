e54dae5afd5826e0d4953dc0cae804ace73b1f7b
Beth Ruff <beth.ruff2@gmail.com>
2020-12-15 20:28:37 -0500

    add hash function to hash table file

---
+++ hashTable/hashFunction.js
@@ -0,0 +1,26 @@
+//below function works on strings only***
+
+function hash(key, arrayLength){
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+        let char = key[i]
+        // map "a" to 1, "b" to 2, "c" to 3, etc.
+        let value = char.charCodeAt(0) - 96
+        total = (total + value) % arrayLength;
+      }
+      return total;
+    }
+
+//refactored + improved function
+
+function hash(key, arrayLength) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i]
+        // map "a" to 1, "b" to 2, "c" to 3, etc.
+        let value = char.charCodeAt(0) - 96
+        total = (total * WEIRD_PRIME + value) % arrayLength;
+      }
+      return total;
+}
