49afd20ee234c3f1ecf4976e366391ae1f37ef33
Beth Ruff <beth.ruff2@gmail.com>
2021-01-06 10:19:27 -0800

    add psuedocode and refactor removeVertex and removeEdge fucntions

--- graphs/removeEdge.js
+++ graphs/removeEdge.js
@@ -1,3 +1,8 @@
+//function should accept two vertices, v1 and v2
+//function should reassign the key of v1 to be an array that does not contain v2
+//function should reassign the key of v2 to be an array that does not contain v1
+//no error handling or invalid vertices
+
 class Graph{
     constructor(){
         this.adjacencyList = {};
@@ -9,12 +14,12 @@ class Graph{
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
-    removeEdge(vertex1,vertex2){
-        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
-            v => v !== vertex2
+    removeEdge(v1,v2){
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v => v !== v2
         );
-        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
-            v => v !== vertex1
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v => v !== v1
         );
     }
 }
--- graphs/removeVertex.js
+++ graphs/removeVertex.js
@@ -1,14 +1,22 @@
+//function should accept a vertex to remove
+//function should lop as long as there are any other verticies in the adjacency list for that vertex
+//inside of the loop, call removeEdge() with the vertex we are removign and any values in the adjacency list for that vertex
+//delete the key in the adjacency list for that vertex
+
 class Graph{
     constructor(){
         this.adjacencyList = {};
     }
+
     addVertex(vertex){
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+
     addEdge(v1,v2){
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
     }
+
     removeEdge(vertex1,vertex2){
         this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
             v => v !== vertex2
@@ -17,6 +25,7 @@ class Graph{
             v => v !== vertex1
         );
     }
+
     removeVertex(vertex){
         while(this.adjacencyList[vertex].length){
             const adjacentVertex = this.adjacencyList[vertex].pop();
@@ -24,4 +33,5 @@ class Graph{
         }
         delete this.adjacencyList[vertex]
     }
+    
 }
