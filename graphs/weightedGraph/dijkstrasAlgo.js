1123f5ff0ecb285934ba90a43ebafb6dc0679cc3
Beth Ruff <beth.ruff2@gmail.com>
2021-01-14 21:40:46 -0800

    add Dijkstras' Algorithm solution

--- graphs/weightedGraph/dijkstrasAlgo.js
+++ graphs/weightedGraph/dijkstrasAlgo.js
@@ -32,7 +32,7 @@ class PriorityQueue {
 }
 
 class WeightedGraph {
-    construtor(){
+    constructor(){
         this.adjacencyList = {};
     }
 
@@ -45,4 +45,55 @@ class WeightedGraph {
         this.adjacencyList[v1].push({node:v2, weight});
         this.adjacencyList[v2].push({node:v1, weight});
     }
+
+    dijkstra(start, finish){
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        let path = []; //to return at end
+        let smallest;
+        //build up initial state
+        for(let vertex in this.adjacencyList){
+            if(vertex === start){
+                distances[vertex] = 0;
+                nodes.enqueue(vertex, 0);
+            } else {
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
+            }
+            previous[vertex] = null;
+        }
+        while(nodes.values.length){
+            smallest = nodes.dequeue().val;
+            if(smallest === finish){
+                //we are done
+                //build up path to return at end
+                while(previous[smallest]){
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+                break;
+            }
+            if(smallest || distances[smallest] !== Infinity) {
+                for(let neighbor in this.adjacencyList[smallest]){
+                    //find neighboring node
+                    let nextNode = this.adjacencyList[smallest][neighbor];
+                    //calculate new distance to neighboring node
+                    let candidate = distances[smallest] + nextNode.weight;
+                    let nextNeighbor = nextNode.node;
+                    if(candidate < distances[nextNeighbor]) {
+                        //updating new smallest distance to neighbor
+                        distances[nextNeighbor] = candidate;
+                        //updating previous - How we got to neighbor
+                        previous[nextNeighbor] = smallest;
+                        //enqueue in priority queue with new priority
+                        nodes.enqueue(nextNeighbor, candidate);
+                    }
+                }
+            }
+        }
+        console.log(path);
+        return path.concat(smallest).reverse();
+    }
+
 }
--- graphs/weightedGraph/weightedGraph.js
+++ graphs/weightedGraph/weightedGraph.js
@@ -1,5 +1,5 @@
 class WeightedGraph {
-    construtor(){
+    constructor(){
         this.adjacencyList = {};
     }
 
